0d152ee7cb4a027dd21ed848810a80d48f2711fe
wwayne <wayneup@163.com>
2015-09-25 12:31:39 +0800

    switch to jsonapi-serializer

--- server.js
+++ server.js
@@ -1,70 +1,43 @@
 'use strict'
 
 const http = require('http')
-const Presenter = require('yayson')().Presenter
-
-class ItemsPresenter extends Presenter {
-
-  constructor() {
-    super();
-    this.type = 'items'
-  }
-
-  attributes(props) {
-    let attrs = super.attributes(props)
-    attrs.start = Date.now()
-    return attrs
-  }  
-
-  relationships() {
-    return {
-      event: EventsPresenter
-    }  
-  }
-
-}
-
-class EventsPresenter extends Presenter {
-
-  constructor() {
-    super();
-    this.type = 'events'
-  }
-
-  attributes(props) {
-    let attrs = super.attributes(props)
-    attrs.ok = 'yes'
-    return attrs
-  } 
-
-  relationships() {
-    return {
-      item: ItemsPresenter
-    } 
-  } 
-
-}
+const JSONAPISerializer = require('jsonapi-serializer');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
     'Content-Types': 'application/json'
   })
 
-  let item = {
+  var data = [{
     id: 1,
-    name: "item1",
-    event: null
-  }
-  let event = {
-    id: 2,
-    name: 'event21',
-    item: item
-  }
-  item.event = event
+    firstName: 'Sandro',
+    lastName: 'Munda',
+    books: {
+      _id: 21,
+      title: 'test', 
+      isbn: 'isbn'
+    }
+  }];
 
-  const json = ItemsPresenter.toJSON(item)
-  console.log(json)
-  res.write(JSON.stringify(json))
+  var users = new JSONAPISerializer('users', data, {
+    topLevelLinks: { self: 'http://localhost:3000/api/users' },
+    attributes: ['firstName', 'lastName', 'books'],
+    books: {
+      ref: '_id',
+      attributes: ['title', 'isbn'],
+      relationshipLinks: {
+        "self": "http://example.com/relationships/books",
+        "related": "http://example.com/books"
+      },
+      includedLinks: {
+        self: function (dataSet, book) {
+          return 'http://example.com/books/' + book.id;
+        }
+      }
+    }
+  });
+
+  res.write(JSON.stringify(users))
   res.end()
 })
 
